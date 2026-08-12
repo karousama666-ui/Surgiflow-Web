0217ef8a18fe4707743c2de3dd364871c8c68e05
Carolina <karousama666@gmail.com>
2026-08-12 15:38:12 -0300

    Médicos ID

--- frontend/src/components/modal/NovaCirurgiaForm.jsx
+++ frontend/src/components/modal/NovaCirurgiaForm.jsx
@@ -1,6 +1,6 @@
 import Input from "../ui/Input";
 import { useState, useEffect } from "react";
-
+import { useMedicos } from "../../context/MedicosContext";
 
 function NovaCirurgiaForm({
 
@@ -10,225 +10,239 @@ function NovaCirurgiaForm({
 
 }) {
 
+    const { listaMedicos } = useMedicos();
+
     const [form, setForm] = useState({
 
-    paciente: "",
-
-    medico: "",
-
-    hospital: "",
-    
-    convenio: "",
-
-    data: "",
-
-    horario: "",
-    
-    anexo: null
-
-});
-
-useEffect(() => {
-
-    if(dados){
-
-        setForm(dados);
-
-    }
-
-}, [dados]);
-
-function handleChange(e){
-
-    const { name, value } = e.target;
-
-    setForm({
-
-        ...form,
-
-        [name]: value
+        paciente: "",
+        medico: "",
+        medicoId: "",
+        hospital: "",
+        convenio: "",
+        data: "",
+        horario: "",
+        anexo: null
 
     });
 
-}
+    useEffect(() => {
 
-console.log(form);
+        if (dados) {
+
+            setForm({
+
+                ...dados,
+
+                medicoId: dados.medicoId || ""
+
+            });
+
+        }
+
+    }, [dados]);
+
+    function handleChange(e) {
+
+        const { name, value } = e.target;
+
+        setForm({
+
+            ...form,
+
+            [name]: value
+
+        });
+
+    }
+
+    function handleMedicoChange(e) {
+
+        const medicoId = e.target.value;
+
+        const medicoSelecionado = listaMedicos.find(
+
+            medico => String(medico.id) === medicoId
+
+        );
+
+        setForm({
+
+            ...form,
+
+            medicoId: medicoId,
+
+            medico: medicoSelecionado
+                ? medicoSelecionado.nome
+                : ""
+
+        });
+
+    }
 
     return (
 
         <>
 
             <Input
+                name="paciente"
+                placeholder="Paciente"
+                value={form.paciente}
+                onChange={handleChange}
+            />
 
-name="paciente"
+            <br />
+            <br />
 
-placeholder="Paciente"
+            <label>
 
-value={form.paciente}
+                Médico
 
-onChange={handleChange}
+            </label>
 
-/>
+            <br />
+
+            <select
+                name="medicoId"
+                value={form.medicoId}
+                onChange={handleMedicoChange}
+            >
+
+                <option value="">
+
+                    Selecione um médico
+
+                </option>
+
+                {listaMedicos.map(medico => (
+
+                    <option
+                        key={medico.id}
+                        value={medico.id}
+                    >
+
+                        {medico.nome}
+
+                    </option>
+
+                ))}
+
+            </select>
 
             <br />
             <br />
 
             <Input
-
-name="medico"
-
-placeholder="Médico"
-
-value={form.medico}
-
-onChange={handleChange}
-
-/>
+                name="hospital"
+                placeholder="Hospital"
+                value={form.hospital}
+                onChange={handleChange}
+            />
 
             <br />
             <br />
 
             <Input
-
-name="hospital"
-
-placeholder="Hospital"
-
-value={form.hospital}
-
-onChange={handleChange}
-
-/>
-
-            <br />
-            <br />
-
-<Input
-
-    name="convenio"
-
-    placeholder="Convênio"
-
-    value={form.convenio}
-
-    onChange={handleChange}
-
-/>
-
-<br />
-<br />
-
-<Input
-
-    name="data"
-
-    type="date"
-
-    value={form.data}
-
-    onChange={handleChange}
-
-/>
+                name="convenio"
+                placeholder="Convênio"
+                value={form.convenio}
+                onChange={handleChange}
+            />
 
             <br />
             <br />
 
             <Input
+                name="data"
+                type="date"
+                value={form.data}
+                onChange={handleChange}
+            />
 
-name="horario"
+            <br />
+            <br />
 
-type="time"
+            <Input
+                name="horario"
+                type="time"
+                value={form.horario}
+                onChange={handleChange}
+            />
 
-value={form.horario}
+            <br />
+            <br />
 
-onChange={handleChange}
+            <label>
 
-/>
+                Anexo
 
-<br />
-<br />
+            </label>
 
-<label>
+            <br />
 
-    Anexo
+            <input
+                type="file"
+                onChange={(e) => {
 
-</label>
+                    setForm({
 
-<br />
+                        ...form,
 
-<input
+                        anexo: e.target.files[0]
 
-    type="file"
+                    });
 
-    onChange={(e) => {
+                }}
+            />
 
-        setForm({
+            <br />
+            <br />
 
-            ...form,
+            {form.anexo && (
 
-            anexo: e.target.files[0]
+                <p
+                    style={{
+                        marginTop: "10px",
+                        color: "#6C63FF",
+                        fontWeight: "600"
+                    }}
+                >
 
-        });
+                    📎 {form.anexo.name}
 
-    }}
+                </p>
 
-/>
+            )}
 
-<br />
+            <button
 
-<br />
+                onClick={() => {
 
-{form.anexo && (
+                    onSave(form);
 
-    <p
-        style={{
-            marginTop: "10px",
-            color: "#6C63FF",
-            fontWeight: "600"
-        }}
-    >
+                    setForm({
 
-        📎 {form.anexo.name}
+                        paciente: "",
+                        medico: "",
+                        medicoId: "",
+                        hospital: "",
+                        convenio: "",
+                        data: "",
+                        horario: "",
+                        anexo: null
 
-    </p>
+                    });
 
-)}
+                }}
 
-<button
+            >
 
-    onClick={() => {
+                Salvar
 
-        onSave(form);
-
-        setForm({
-
-            paciente: "",
-
-            medico: "",
-
-            hospital: "",
-
-            convenio: "",
-
-            data: "",
-
-            horario: "",
-
-            anexo: null
-
-        });
-
-    }}
-
->
-
-Salvar
-
-</button>
+            </button>
 
         </>
 
-    )
+    );
 
 }
 
