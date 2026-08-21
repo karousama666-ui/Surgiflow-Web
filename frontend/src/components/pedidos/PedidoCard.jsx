d9bdb1077fc728203540facc97f314539668d28d
Carolina <karousama666@gmail.com>
2026-08-21 10:16:34 -0300

    ALTERAÇÃO PEDIDO MODAL

--- frontend/src/components/pedidos/PedidoCard.jsx
+++ frontend/src/components/pedidos/PedidoCard.jsx
@@ -73,7 +73,8 @@ function PedidoCard({ cirurgia, pedido, onPreview }) {
             <div className="pedido-actions">
                 <button type="button" onClick={onPreview} style={{ width: "100%", justifyContent: "center" }}>
                     <Eye size={17} /> 
-                    <span>Gerenciar OPME / PDF</span>
+                    {/* 👇 TEXTO ATUALIZADO 👇 */}
+                    <span>Gerenciar OPME / Anexos</span>
                 </button>
             </div>
         </div>
